1d68877500eb65110b39fd76fa031caf737afe55
Ashraf-DIU <ashrafbinalam17@gmail.com>
2024-09-05 10:33:09 +0600

    first commit

---
+++ oops.java
@@ -0,0 +1,17 @@
+class pen{
+    String color;
+    String type; //ballpoint,gel
+    public void write(){
+        System.out.println("Writting Something");
+    }
+}
+
+public class oops {
+    public static void main(String[] args) {
+        pen pen1 = new pen();
+        pen1.color = "blue";
+        pen1.type = "gel";
+
+        pen1.write();
+    }
+}
